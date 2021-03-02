bfd0fe2f8ee655a998d9ac10f73503fa3a60370e
Andrew Chung <590100+andrewchung@users.noreply.github.com>
2021-03-02 17:00:50 -0500

    submission Bite 54 @ codechalleng.es

--- 54/poem.py
+++ 54/poem.py
@@ -1,14 +1,41 @@
 import textwrap
 
-INDENTS = 4
 
-poem =  """
-        He took a sip of the drink. He wasn't sure whether he liked it or not, but at this moment it didn't matter. She had made it especially for him so he would have forced it down even if he had absolutely hated it. That's simply the way things worked. She made him a new-fangled drink each day and he took a sip of it and smiled, saying it was excellent.
-        """
+INDENTS = 4
+INDENTS_STR = INDENTS * ' '
+
+shakespeare_unformatted = """
+                          To be, or not to be, that is the question:
+                          Whether 'tis nobler in the mind to suffer
+
+                          The slings and arrows of outrageous fortune,
+                          Or to take Arms against a Sea of troubles,
+                          """
+rosetti_unformatted = """
+                      Remember me when I am gone away,
+                      Gone far away into the silent land;
+                      When you can no more hold me by the hand,
+
+                      Nor I half turn to go yet turning stay.
+
+                      Remember me when no more day by day
+                      You tell me of our future that you planned:
+                      Only remember me; you understand
+                      """
+
+poem = shakespeare_unformatted
+
 
 def print_hanging_indents(poem):
-    print(poem)
-    print(textwrap.shorten(poem, width=30))
-    pass
+
+    first_split = []
+    first_split = poem.split('\n\n')
+
+    for item in first_split:
+        item = item.lstrip()
+
+        print(textwrap.fill(item, expand_tabs=True, width=50, tabsize=0,
+                            initial_indent='', subsequent_indent=INDENTS_STR))
+
 
 print_hanging_indents(poem)
