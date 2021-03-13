eb67ad254aab0802c7b748a662a77cd6b5e1582b
Andrew Chung <590100+andrewchung@users.noreply.github.com>
2021-03-13 12:38:22 -0500

    submission Bite 66 @ codechalleng.es

---
+++ 66/running_mean.py
@@ -0,0 +1,17 @@
+import itertools
+sequence = []
+mean = []
+
+
+def running_mean(sequence):
+    """Calculate the running mean of the sequence passed in,
+       returns a sequence of same length with the averages.
+       You can assume all items in sequence are numeric."""
+    sum = 0
+    for i, num in enumerate(sequence, start=1):
+        sum += num
+        mean.append(round(sum / i, 2))
+    print(mean)
+
+
+running_mean(sequence)
